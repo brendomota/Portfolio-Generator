44f1e2bac89209b8c54cba29ecc901c519d37f19
brendomota <brendo.saescola@gmail.com>
2026-04-07 11:21:11 -0300

    feat: configurar aplicação principal para se conectar com pastas de model e repository, além de configurar properties para conexão ao banco de dados.

--- backend/src/main/java/br/unesp/backend/BackendApplication.java
+++ backend/src/main/java/br/unesp/backend/BackendApplication.java
@@ -1,9 +1,23 @@
 package br.unesp.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"br.unesp.backend.model"})
+@ComponentScan(basePackages = {"br.*"})
+@EnableJpaRepositories(basePackages = {"br.unesp.backend.repository"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class BackendApplication {
 
 	public static void main(String[] args) {
