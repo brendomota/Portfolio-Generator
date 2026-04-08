6ed15f318460e226ef81cfb538fdc7f6cef50c46
brendomota <brendo.saescola@gmail.com>
2026-04-08 16:21:04 -0300

    feat(Usuario.java): criar classe para usuário, com id, nome, email e senha.

---
+++ backend/src/main/java/br/unesp/backend/model/Usuario.java
@@ -0,0 +1,30 @@
+package br.unesp.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+}
