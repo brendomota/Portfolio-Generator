0f8c43ffb4152d0bc8616bae7ef7bc12aed4210f
brendomota <brendo.saescola@gmail.com>
2026-04-10 11:07:53 -0300

    feat: adicionar método Post em UsuarioController.java, testar e garantir que persistiu no banco de dados.

--- backend/src/main/java/br/unesp/backend/controller/UsuarioController.java
+++ backend/src/main/java/br/unesp/backend/controller/UsuarioController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.unesp.backend.model.Usuario;
@@ -34,6 +36,15 @@ public class UsuarioController {
                 
         Optional<Usuario> usuario = usuarioRepository.findById(id);
         return new ResponseEntity<>(usuario.get(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Usuario> saveUsuario (
+        @RequestBody Usuario usuario
+    ){
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+        return new ResponseEntity<>(usuarioSalvo, HttpStatus.OK);
     }
 
 }
